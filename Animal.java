c54ad36c49ee9d4d3b1cb3b6965812066c040e0c
LucL <levesqueluc87@gmail.com>
2022-04-08 10:11:32 -0400

    added basic animal traits

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public abstract class Animal{
+
+  public abstract int numberOfLegs();
+
+  public abstract string species();
+
+  public abstract int numberOfEyes();
+
+
+  
+}
---
+++ AnimalTraits.java
@@ -0,0 +1,13 @@
+public abstract interface AnimalTraits{
+
+  public abstract boolean hasClaws();
+
+  public abstract boolean canClimb();
+
+  public abstract boolean canFly();
+
+  public abstract boolean haveTail();
+
+  
+  
+}
